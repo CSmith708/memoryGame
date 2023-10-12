3173e2d9800256575f9b0661dc8aba2788a2a7b9
CSmith708 <cds5764@psu.edu>
2023-10-11 23:26:28 -0400

    Added timer and if-else conditions

--- MemoryGame.java
+++ MemoryGame.java
@@ -3,10 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-
-
 
 public class MemoryGame extends JFrame {
     private ArrayList<String> imagePaths;
@@ -18,7 +15,7 @@ public class MemoryGame extends JFrame {
 
     public MemoryGame() {
         setTitle("Picture Memory Game");
-        setSize(400,400);
+        setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         imagePaths = new ArrayList<>();
@@ -56,34 +53,68 @@ public class MemoryGame extends JFrame {
         Collections.shuffle(imagePaths);
         Collections.shuffle(cardImages);
 
-        JPanel cardPannel = new JPanel(new GridLayout(4,4));
+        JPanel cardPannel = new JPanel(new GridLayout(4, 6));
         cardButtons = new JButton[24];
 
-        for (int i=0; i < cardButtons.length; i++) {
+        for (int i = 0; i < cardButtons.length; i++) {
             final int index = i;
             cardButtons[i] = new JButton();
             cardButtons[i].setIcon(new ImageIcon("presidents/potusSeal.png"));
             cardButtons[i].addActionListener(new ActionListener() {
                 @Override
-                public void actionPerformed(ActionEvent e) { handleCardClick(index); }
+                public void actionPerformed(ActionEvent e) {
+                    handleCardClick(index);
+                }
             });
             cardPannel.add(cardButtons[i]);
         }
-
-        add(cardPanel);
-
+        add(cardPannel);
     }
 
+    private void handleCardClick(int index) {
+        if (cardButtons[index].getIcon() == null) {
+            return;
+        }
 
+        if (firstCardIndex == -1) {
+            firstCardIndex = index;
+            cardButtons[firstCardIndex].setIcon(new ImageIcon(imagePaths.get(index)));
+        } else {
+            secondCardIndex = index;
+            cardButtons[secondCardIndex].setIcon(new ImageIcon(imagePaths.get(index)));
+        }
 
+        Timer timer = new Timer(500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (imagePaths.get(firstCardIndex).equals(imagePaths.get(secondCardIndex))) {
+                    cardButtons[firstCardIndex].setIcon(null);
+                    cardButtons[secondCardIndex].setIcon(null);
+                    cardImages.set(firstCardIndex, null);
+                    cardImages.set(secondCardIndex, null);
+                    numberOfMatches++;
 
-
+                    if (numberOfMatches == imagePaths.size() / 2) {
+                        JOptionPane.showMessageDialog(null, "Congratualtions, You Finished The Game!!!");
+                        System.exit(0);
+                    } else {
+                        cardButtons[firstCardIndex].setIcon(new ImageIcon("presidents/potusSeal.png"));
+                        cardButtons[secondCardIndex].setIcon(new ImageIcon("presidents/potusSeal.png"));
+                    }
+                    firstCardIndex = -1;
+                }
+            }
+        });
+        timer.setRepeats(false);
+        timer.start();
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
-            public void run() {new MemoryGame().setVisible(true);}
+            public void run() {
+                new MemoryGame().setVisible(true);
+            }
         });
     }
-
 }
