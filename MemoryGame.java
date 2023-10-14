2a3b40c134a998a8ed829a87698e67e3f5abdd39
CSmith708 <cds5764@psu.edu>
2023-10-14 12:31:21 -0400

    Added user input for grid size with variables and looping structure to set gridSize and number of cards

--- MemoryGame.java
+++ MemoryGame.java
@@ -5,13 +5,27 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 
+
 public class MemoryGame extends JFrame {
     private ArrayList<String> imagePaths;
     private ArrayList<String> cardImages;
     private JButton[] cardButtons;
     private int numberOfMatches;
-    private int firstCardIndex;
+    private int firstCardIndex = -1;
     private int secondCardIndex;
+
+    private int gridRow;
+    private int gridCol;
+    private int gridButtons;
+    private static boolean completionStatus = false;
+
+
+    //JOptionPane to get user input on grid size. Code from JavaDoc https://docs.oracle.com/javase/8/docs/api/javax/swing/JOptionPane.html
+    Object[] possibleValues = { "Twelve", "Twenty-Four"};
+    Object gridSize = JOptionPane.showInputDialog(null,
+            "Choose your grid size", "Game Initialization",
+            JOptionPane.QUESTION_MESSAGE, null,
+            possibleValues, possibleValues[0]);
 
     public MemoryGame() {
         setTitle("Picture Memory Game");
@@ -20,30 +34,27 @@ public class MemoryGame extends JFrame {
 
         imagePaths = new ArrayList<>();
         //add file paths to images
-        imagePaths.add("presidents/president1.png");
-        imagePaths.add("presidents/president2.png");
-        imagePaths.add("presidents/president3.png");
-        imagePaths.add("presidents/president4.png");
-        imagePaths.add("presidents/president5.png");
-        imagePaths.add("presidents/president6.png");
-        imagePaths.add("presidents/president7.png");
-        imagePaths.add("presidents/president8.png");
-        imagePaths.add("presidents/president9.png");
-        imagePaths.add("presidents/president10.png");
-        imagePaths.add("presidents/president11.png");
-        imagePaths.add("presidents/president12.png");
-        imagePaths.add("presidents/president1.png");
-        imagePaths.add("presidents/president2.png");
-        imagePaths.add("presidents/president3.png");
-        imagePaths.add("presidents/president4.png");
-        imagePaths.add("presidents/president5.png");
-        imagePaths.add("presidents/president6.png");
-        imagePaths.add("presidents/president7.png");
-        imagePaths.add("presidents/president8.png");
-        imagePaths.add("presidents/president9.png");
-        imagePaths.add("presidents/president10.png");
-        imagePaths.add("presidents/president11.png");
-        imagePaths.add("presidents/president12.png");
+        switch (gridSize.toString()) {
+            case "Twelve" -> {
+                for (int i = 1; i < 7; i++) {
+                    imagePaths.add("presidents/president" + i + ".png");
+                    imagePaths.add("presidents/president" + i + ".png");
+
+                }
+                gridRow = 4;
+                gridCol = 3;
+                gridButtons = 12;
+            }
+            case "Twenty-Four" -> {
+                for (int i = 1; i < 13; i++) {
+                    imagePaths.add("presidents/president" + i + ".png");
+                    imagePaths.add("presidents/president" + i + ".png");
+                }
+                gridRow = 4;
+                gridCol = 6;
+                gridButtons = 24;
+            }
+        }
 
         cardImages = new ArrayList<>();
         for (String imagePath : imagePaths) {
@@ -53,8 +64,10 @@ public class MemoryGame extends JFrame {
         Collections.shuffle(imagePaths);
         Collections.shuffle(cardImages);
 
-        JPanel cardPannel = new JPanel(new GridLayout(4, 6));
-        cardButtons = new JButton[24];
+
+
+        JPanel cardPannel = new JPanel(new GridLayout(gridRow, gridCol));
+        cardButtons = new JButton[gridButtons];
 
         for (int i = 0; i < cardButtons.length; i++) {
             final int index = i;
@@ -68,7 +81,7 @@ public class MemoryGame extends JFrame {
             });
             cardPannel.add(cardButtons[i]);
         }
-        add(cardPannel);
+    add(cardPannel);
     }
 
     private void handleCardClick(int index) {
@@ -83,10 +96,10 @@ public class MemoryGame extends JFrame {
             secondCardIndex = index;
             cardButtons[secondCardIndex].setIcon(new ImageIcon(imagePaths.get(index)));
         }
-
-        Timer timer = new Timer(500, new ActionListener() {
+            Timer timer = new Timer(5000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
                 if (imagePaths.get(firstCardIndex).equals(imagePaths.get(secondCardIndex))) {
                     cardButtons[firstCardIndex].setIcon(null);
                     cardButtons[secondCardIndex].setIcon(null);
@@ -96,25 +109,28 @@ public class MemoryGame extends JFrame {
 
                     if (numberOfMatches == imagePaths.size() / 2) {
                         JOptionPane.showMessageDialog(null, "Congratualtions, You Finished The Game!!!");
+                        completionStatus = true;
                         System.exit(0);
-                    } else {
-                        cardButtons[firstCardIndex].setIcon(new ImageIcon("presidents/potusSeal.png"));
-                        cardButtons[secondCardIndex].setIcon(new ImageIcon("presidents/potusSeal.png"));
                     }
-                    firstCardIndex = -1;
+                } else {
+                    cardButtons[firstCardIndex].setIcon(new ImageIcon("presidents/potusSeal.png"));
+                    cardButtons[secondCardIndex].setIcon(new ImageIcon("presidents/potusSeal.png"));
                 }
+                firstCardIndex = -1;
             }
-        });
-        timer.setRepeats(false);
-        timer.start();
+            });
+            timer.setRepeats(false);
+            timer.restart();
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new MemoryGame().setVisible(true);
-            }
+                        new MemoryGame().setVisible(true);
+                    }
         });
+
+        }
+
     }
-}
